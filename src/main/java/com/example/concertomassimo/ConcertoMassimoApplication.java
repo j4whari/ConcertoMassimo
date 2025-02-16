ad31cbd7e4fbd1ee8b2a3a2fc4d3cdd492e32e75
bansheett <avecesare01@gmail.com>
2025-02-16 18:00:19 +0100

    First commit

---
+++ src/main/java/com/example/concertomassimo/ConcertoMassimoApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertomassimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertoMassimoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertoMassimoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/concertomassimo/ConcertoMassimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concertomassimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertoMassimoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
